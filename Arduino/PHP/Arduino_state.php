e7ab74d473c24f22de1650e1809965cf72a90285
Mickey <106021054@live.asia.edu.tw>
2019-10-24 09:46:04 +0800

    更新PHP

--- Arduino/PHP/Arduino_state.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'connection.php';
-    $device = $_REQUEST['device'];
-    $result=mysqli_query($con,"SELECT * FROM $device");
-    $number_of_rows = mysqli_num_rows($result);
-    while($row = mysqli_fetch_array($result)){
-        echo $row["state"];
-    }
-?>
