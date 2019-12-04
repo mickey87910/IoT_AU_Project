ee7a6c28c29412d6ef09aaf3e992482b30450afe
omega87910 <106021054@live.asia.edu.tw>
2019-12-05 02:38:17 +0800

    可以顯示icon 但 ios 無法build

--- APP/App.js
+++ APP/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+import IconFA from 'react-native-vector-icons/FontAwesome';
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <IconFA name="thermometer-3" size={64} color="red" />
         <Text>即時顯示溫濕度資料</Text>
+        <IconMCI name="water-percent" size={80} color="blue"></IconMCI>
       </View>
     );
   }
