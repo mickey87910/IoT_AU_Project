d1f1f222bef32b565e24b6f1b4d6f72319a9fcb7
omega87910 <106021054@live.asia.edu.tw>
2019-12-05 11:43:19 +0800

    增加Grid layout

--- APP/App.js
+++ APP/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,StyleSheet} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import IconFA from 'react-native-vector-icons/FontAwesome';
 import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Grid,Row,Col} from 'react-native-easy-grid';
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <IconFA name="thermometer-3" size={64} color="red" />
-        <Text>即時顯示溫濕度資料</Text>
-        <IconMCI name="water-percent" size={80} color="blue"></IconMCI>
+      <View style={{ flex: 1}}>
+        <Grid>
+          <Row size={20}>
+            <Col style={styles.top}>
+              <IconFA name="thermometer-3" size={48} color="red">溫度</IconFA>
+            </Col>
+            <Col style={styles.top}>
+              <IconMCI name="water-percent" size={48} color="blue">濕度</IconMCI>
+            </Col>
+          </Row>
+          <Row size={15} >
+            <Col>
+              <Text style={styles.text}>30</Text>
+            </Col>
+            <Col>
+              <Text style={styles.text}>80</Text>
+            </Col>
+          </Row>
+          <Row size={65} style={styles.content}>
+            <Text>123</Text>
+          </Row>
+        </Grid>
       </View>
     );
   }
@@ -30,5 +49,18 @@ const TabNavigator = createBottomTabNavigator({
   主頁面: HomeScreen,
   歷史資料庫: SettingsScreen,
 });
-
+const styles = StyleSheet.create({
+  top:{
+    justifyContent:'flex-end',
+    alignItems:'center',
+  },
+  text:{
+    textAlign:'center',
+    fontSize:64,
+  },
+  content:{
+    justifyContent:'center',
+    alignItems:'center',
+  },
+});
 export default createAppContainer(TabNavigator);
